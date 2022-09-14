d1b9bb840d7082ab02d5575213af45871a0d81c6
Mahmoudd777 <103339082+Mahmoudd777@users.noreply.github.com>
2022-09-15 01:10:14 +0200

    Add files via upload

---
+++ website/app.js
@@ -0,0 +1,102 @@
+// Creating a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.toDateString();
+
+// The URL to retrieve weather information from his API (country : US)
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+
+// // Personal API Key for OpenWeatherMap API
+// &units=metric to get the Celsius Temperature
+const apiKey = ",&appid=c21ffda219b5423e0553f07858a6b0d8&units=metric";
+
+// the URL of the server to post data
+const server = "http://127.0.0.1:4000";
+
+// showing the error to the user
+const error = document.getElementById("error");
+// genrate data
+const generateData = () => { 
+  //get values
+  const zip = document.getElementById("zip").value;
+  const feelings = document.getElementById("feelings").value;
+
+  // getWeatherData 
+  getWeatherData(zip).then((data) => {
+    if (data) {
+      const {
+        main: { temp },
+        name: city,
+        weather: [{ description }],
+      } = data;
+      const info = {
+        newDate,
+        city,
+        temp: Math.round(temp), 
+        description,
+        feelings,
+      };
+
+      postData(server + "/add", info);
+
+      updatingUI();
+      document.getElementById('entry').style.opacity = 1;
+    }
+  });
+};
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById("generate").addEventListener("click", generateData);
+
+//Function to GET Web API Data
+const getWeatherData = async (zip) => {
+  try {
+    const res = await fetch(baseURL + zip + apiKey);
+    const data = await res.json();
+
+    if (data.cod != 200) {
+      // display the error message on UI
+      error.innerHTML = data.message;
+      setTimeout(_=> error.innerHTML = '', 2000)
+      throw `${data.message}`;
+    }
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Function to POST data
+const postData = async (url = "", info = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(info),
+  });
+
+  try {
+    const newData = await res.json();
+    console.log(`You just saved`, newData);
+    return newData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//Function to GET Project Data
+const updatingUI = async () => {
+  const res = await fetch(server + "/all");
+  try {
+    const savedData = await res.json();
+
+    document.getElementById("date").innerHTML = savedData.newDate;
+    document.getElementById("city").innerHTML = savedData.city;
+    document.getElementById("temp").innerHTML = savedData.temp + '&degC';
+    document.getElementById("description").innerHTML = savedData.description;
+    document.getElementById("content").innerHTML = savedData.feelings;
+  } catch (error) {
+    console.log(error);
+  }
+};
